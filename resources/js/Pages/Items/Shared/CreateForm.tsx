7a3c4dae5b655a3135d1b26c3082bbfcf5b41f08
Alfredo Martínez <martin3zra@gmail.com>
2025-06-14 07:56:46 -0400

    improve item form layout

--- resources/js/Pages/Items/Shared/CreateForm.tsx
+++ resources/js/Pages/Items/Shared/CreateForm.tsx
@@ -1,5 +1,6 @@
+import ActionSection from '@/components/action-section';
 import { ConfirmsPassword } from '@/components/confirms-password';
-import HeadingSmall from '@/components/heading-small';
+import FormSection from '@/components/form-section';
 import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -12,7 +13,7 @@ import { Item, ItemType, ItemTypes, PageProps, Tax, Unit, Verb } from '@/types';
 import { Field, Radio, RadioGroup } from '@headlessui/react';
 import { useForm, usePage } from '@inertiajs/react';
 import { CheckCircleIcon } from 'lucide-react';
-import { FormEventHandler, useState } from 'react';
+import { useState } from 'react';
 
 export type CreateFormParams = {
   item: Item | undefined;
@@ -65,173 +66,170 @@ export default function CreateForm({ onFinish, params }: CreateFormProps) {
 
   const verbName = useVerb().action(params.action);
 
-  const submit: FormEventHandler = (e) => {
-    e.preventDefault();
-
-    if (params.action === 'create') post('/items', options);
-    if (params.action === 'edit') put(`/items/${params.item!.id}`, options);
+  const submit = () => {
+    // if (params.action === 'create') {
+    //   post('/items', options)
+    //   return
+    // };
+    // if (params.action === 'edit') put(`/items/${params.item!.id}`, options);
   };
 
   return (
-    <div>
-      {propsErrors.status && <div className="mb-4 text-center text-sm font-medium text-red-600">{propsErrors.status}</div>}
-      <form onSubmit={submit} className="space-y-6">
-        <Label htmlFor="name">{t('items.single.type')}</Label>
-        <div className="col-span-2">
-          <RadioGroup className="grid grid-cols-3 gap-6" value={data.item_type} onChange={(type: ItemType) => setData('item_type', type)}>
-            {ItemTypes.map((type: ItemType) => (
-              <Field key={type}>
-                <Radio
-                  value={type}
-                  className="group data-checked:bg-primary data-checked:text-primary-foreground bg-primary/5 data-focus:outline-primary relative flex cursor-pointer grid-cols-1 rounded-lg px-5 py-4 shadow-md transition focus:not-data-focus:outline-none data-focus:outline"
-                >
-                  <div className="flex w-full capitalize">{t(`items.single.${type}`)}</div>
-                  <CheckCircleIcon className="size-6 opacity-0 transition group-data-checked:opacity-100" />
-                </Radio>
-              </Field>
-            ))}
-          </RadioGroup>
-        </div>
-        <div className="grid grid-cols-2 gap-2">
-          <div className="grid gap-2">
-            <Label htmlFor="name">{t('global.name')}</Label>
-            <Input
-              id="name"
-              className="mt-1 block w-full"
-              value={data.name}
-              onChange={(e) => setData('name', e.target.value)}
-              required
-              autoComplete="name"
-              placeholder="Item name"
-              readOnly={viewMode}
-            />
-            <InputError className="mt-2" message={errors.name} />
-          </div>
-          <div className="grid gap-2">
-            <Label>{t('global.unit')}</Label>
-            <Select
-              onValueChange={(value) => setData('unit_id', Number(value))}
-              disabled={viewMode}
-              defaultValue={data.unit_id.toString()}
-              value={data.unit_id.toString()}
-              required
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Select item unit" />
-              </SelectTrigger>
-              <SelectContent className="">
-                {params.units.map((unit) => (
-                  <SelectItem key={unit.id} value={unit.id.toString()}>
-                    {unit.name}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <InputError className="mt-2" message={errors.unit_id} />
-          </div>
-        </div>
-        <div className="grid grid-cols-2 gap-2">
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col gap-2">
-              <Label>{t('global.tax')}</Label>
-              <Select
-                onValueChange={(value) => setData('tax_id', Number(value))}
-                disabled={viewMode}
-                defaultValue={data.tax_id.toString()}
-                value={data.tax_id.toString()}
-                required
-              >
-                <SelectTrigger className="w-44">
-                  <SelectValue placeholder="Select item tax" />
-                </SelectTrigger>
-                <SelectContent className="">
-                  {params.taxes.map((tax) => (
-                    <SelectItem key={tax.id} value={tax.id.toString()}>
-                      {tax.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <InputError className="mt-2" message={errors.tax_id} />
-            </div>
+    <div className="flex flex-col space-y-6">
+      <FormSection onSubmit={submit}>
+        <FormSection.Title>{t('items.single.title')}</FormSection.Title>
+        <FormSection.Description>{t('items.single.description')}</FormSection.Description>
+        <FormSection.Form>
+          {propsErrors.status && <div className="mb-4 text-center text-sm font-medium text-red-600">{propsErrors.status}</div>}
 
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="price">{t('global.price')}</Label>
-              <Input
-                id="price"
-                type="number"
-                className="mt-0 block w-full max-w-40 text-right"
-                value={data.price}
-                onChange={(e) => setData('price', e.target.valueAsNumber)}
-                placeholder="Jane Doe"
-                readOnly={viewMode}
-              />
-              <InputError className="mt-2" message={errors.price} />
+          <div className="col-span-6 flex flex-col gap-y-6">
+            <div className="col-span-6 flex flex-col gap-2">
+              <Label htmlFor="name">{t('items.single.type')}</Label>
+              <RadioGroup className="grid grid-cols-3 gap-6" value={data.item_type} onChange={(type: ItemType) => setData('item_type', type)}>
+                {ItemTypes.map((type: ItemType) => (
+                  <Field key={type}>
+                    <Radio
+                      value={type}
+                      className="group data-checked:bg-primary data-checked:text-primary-foreground bg-primary/5 data-focus:outline-primary relative flex cursor-pointer grid-cols-1 rounded-lg px-5 py-4 shadow-md transition focus:not-data-focus:outline-none data-focus:outline"
+                    >
+                      <div className="flex w-full capitalize">{t(`items.single.${type}`)}</div>
+                      <CheckCircleIcon className="size-6 opacity-0 transition group-data-checked:opacity-100" />
+                    </Radio>
+                  </Field>
+                ))}
+              </RadioGroup>
+            </div>
+            <div className="col-span-6 flex flex-col gap-2">
+              <div className="gap-2">
+                <Label htmlFor="name">{t('global.name')}</Label>
+                <Input
+                  id="name"
+                  className="mt-1 block w-full"
+                  value={data.name}
+                  onChange={(e) => setData('name', e.target.value)}
+                  required
+                  autoComplete="name"
+                  placeholder="Item name"
+                  readOnly={viewMode}
+                />
+                <InputError className="mt-2" message={errors.name} />
+              </div>
+            </div>
+            <div className="col-span-6 flex flex-col gap-2">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-2">
+                  <Label>{t('global.unit')}</Label>
+                  <Select
+                    onValueChange={(value) => setData('unit_id', Number(value))}
+                    disabled={viewMode}
+                    defaultValue={data.unit_id.toString()}
+                    value={data.unit_id.toString()}
+                    required
+                  >
+                    <SelectTrigger className="w-44">
+                      <SelectValue placeholder="Select item unit" />
+                    </SelectTrigger>
+                    <SelectContent className="">
+                      {params.units.map((unit) => (
+                        <SelectItem key={unit.id} value={unit.id.toString()}>
+                          {unit.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <InputError className="mt-2" message={errors.unit_id} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label>{t('global.tax')}</Label>
+                  <Select
+                    onValueChange={(value) => setData('tax_id', Number(value))}
+                    disabled={viewMode}
+                    defaultValue={data.tax_id.toString()}
+                    value={data.tax_id.toString()}
+                    required
+                  >
+                    <SelectTrigger className="w-44">
+                      <SelectValue placeholder="Select item tax" />
+                    </SelectTrigger>
+                    <SelectContent className="">
+                      {params.taxes.map((tax) => (
+                        <SelectItem key={tax.id} value={tax.id.toString()}>
+                          {tax.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <InputError className="mt-2" message={errors.tax_id} />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="price">{t('global.price')}</Label>
+                  <Input
+                    id="price"
+                    type="number"
+                    className="mt-0 block w-full max-w-40 text-right"
+                    value={data.price}
+                    onChange={(e) => setData('price', e.target.valueAsNumber)}
+                    placeholder="Jane Doe"
+                    readOnly={viewMode}
+                  />
+                  <InputError className="mt-2" message={errors.price} />
+                </div>
+              </div>
+            </div>
+            <div className="col-span-6 flex flex-col gap-2">
+              <div className="grid gap-2">
+                <Label htmlFor="description">{t('global.description')}</Label>
+                <textarea
+                  id="description"
+                  className="mt-1 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                  value={data.description}
+                  onChange={(e) => setData('description', e.target.value)}
+                  placeholder="Wrire some description here..."
+                  rows={3}
+                  readOnly={viewMode}
+                />
+                <InputError className="mt-2" message={errors.description} />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="grid grid-cols-2 gap-2">
-          <div className="grid gap-2">
-            <Label htmlFor="description">{t('global.description')}</Label>
-            <textarea
-              id="description"
-              className="mt-1 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-              value={data.description}
-              onChange={(e) => setData('description', e.target.value)}
-              placeholder="Wrire some description here..."
-              rows={3}
-              readOnly={viewMode}
-            />
-            <InputError className="mt-2" message={errors.description} />
-          </div>
-          {/* <div className="grid gap-2">
-            <Label htmlFor="phone">Phone</Label>
-            <Input
-              id="phone"
-              className="mt-1 block w-full"
-              value={data.phone}
-              onChange={(e) => setData('phone', e.target.value)}
-              placeholder="809-983-3897"
-              readOnly={viewMode}
-            />
-            <InputError className="mt-2" message={errors.phone} />
-          </div> */}
-        </div>
+        </FormSection.Form>
         {!viewMode && (
-          <div className="flex items-center gap-4">
-            <Button disabled={processing}>
-              {t(`global.actions.${verbName}`)} {t('global.item')}
-            </Button>
-          </div>
+          <FormSection.Actions>
+            <div className="flex items-center gap-4">
+              <Button disabled={processing} className="uppercase">
+                {t(`global.actions.${verbName}`)} {t('global.item')}
+              </Button>
+            </div>
+          </FormSection.Actions>
         )}
-      </form>
+      </FormSection>
 
       {viewMode && (
-        <div className="space-y-6 pt-12">
-          <HeadingSmall
-            title={t(`items.statuses.${params.item?.status || 'enabled'}.section.title`)}
-            description={t(`items.statuses.${params.item?.status || 'enabled'}.section.description`)}
-          />
-          <div className={`space-y-4 rounded-lg border ${isDisabled ? 'border-primary-100 bg-primary-50' : 'border-red-100 bg-red-50'} p-4`}>
-            <div className={`relative space-y-0.5 ${isDisabled ? 'text-primary' : 'text-red-600'}`}>
-              <p className="font-medium">{t('global.warning.title')}</p>
-              <p className="text-sm">{t('global.warning.description')}</p>
-            </div>
-            <Button variant={isDisabled ? 'default' : 'destructive'} onClick={() => setDialogOpen(true)}>
-              {t(`items.statuses.${params.item?.status || 'enabled'}.section.title`)}
-            </Button>
+        <ActionSection>
+          <ActionSection.Title>{t(`items.statuses.${params.item?.status || 'enabled'}.section.title`)}</ActionSection.Title>
+          <ActionSection.Description>{t(`items.statuses.${params.item?.status || 'enabled'}.section.description`)}</ActionSection.Description>
+          <ActionSection.Content>
+            <div className={`space-y-2 rounded-lg border ${isDisabled ? 'border-primary-100 bg-primary-50' : 'border-red-100 bg-red-50'} p-4`}>
+              <div className={`relative space-y-0.5 ${isDisabled ? 'text-primary' : 'text-red-600'}`}>
+                <p className="font-medium">{t('global.warning.title')}</p>
+                <p className="text-sm">{t('global.warning.description')}</p>
+              </div>
+              <Button variant={isDisabled ? 'default' : 'destructive'} onClick={() => setDialogOpen(true)}>
+                {t(`items.statuses.${params.item?.status || 'enabled'}.section.title`)}
+              </Button>
 
-            <ConfirmsPassword
-              title={t(`items.statuses.${params.item?.status || 'enabled'}.confirmsPassword.title`, { item: params.item?.name || '' })}
-              description={t(`items.statuses.${params.item?.status || 'enabled'}.confirmsPassword.description`)}
-              action={t(`items.statuses.${params.item?.status || 'enabled'}.confirmsPassword.confirm`)}
-              verb={'update'}
-              path={`/items/${params.item?.id}/change-status`}
-              open={dialogOpen}
-              onOpenChange={setDialogOpen}
-            />
-          </div>
-        </div>
+              <ConfirmsPassword
+                title={t(`items.statuses.${params.item?.status || 'enabled'}.confirmsPassword.title`, { item: params.item?.name || '' })}
+                description={t(`items.statuses.${params.item?.status || 'enabled'}.confirmsPassword.description`)}
+                action={t(`items.statuses.${params.item?.status || 'enabled'}.confirmsPassword.confirm`)}
+                verb={'update'}
+                path={`/items/${params.item?.id}/change-status`}
+                open={dialogOpen}
+                onOpenChange={setDialogOpen}
+              />
+            </div>
+          </ActionSection.Content>
+        </ActionSection>
       )}
     </div>
   );
