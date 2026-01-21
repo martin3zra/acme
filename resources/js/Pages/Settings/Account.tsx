53cc6e46dbfbe519b34ae737cb9ee3576a25f860
Alfredo Martínez <martin3zra@gmail.com>
2026-01-21 00:17:41 -0400

    Fix modal issue

--- resources/js/Pages/Settings/Account.tsx
+++ resources/js/Pages/Settings/Account.tsx
@@ -139,60 +139,60 @@ export default function Account({
   };
   return (
     <AppLayout breadcrumbs={breadcrumbs} user={auth.user}>
-      <div className="flex gap-x-1">
-        <div className="flex basis-[30vw] flex-col gap-y-6 py-6">
-          <div className="flex items-end gap-6">
-            <div className="relative flex size-22 items-center">
-              <Avatar className="bg-muted flex h-22 w-22 items-center justify-center rounded-full">
-                <AvatarFallback className="rounded-lg text-4xl">{getInitials(auth.user.name)}</AvatarFallback>
-              </Avatar>
-
-              {auth.user.email_verified_at !== null && <BadgeCheck size={22} className="absolute right-0 bottom-2" />}
-            </div>
-            <div className="mb-2">
-              <div className="flex items-end gap-2">
-                <h1 className="text-2xl">{auth.user.name}</h1>
-                <StatusBadge type="status" status={auth.user.status} />
+      <div className="space-y-6">
+        <div className="flex gap-x-1">
+          <div className="flex basis-[30vw] flex-col gap-y-6 py-6">
+            <div className="flex items-end gap-6">
+              <div className="relative flex size-22 items-center">
+                <Avatar className="bg-muted flex h-22 w-22 items-center justify-center rounded-full">
+                  <AvatarFallback className="rounded-lg text-4xl">{getInitials(auth.user.name)}</AvatarFallback>
+                </Avatar>
+                {auth.user.email_verified_at !== null && <BadgeCheck size={22} className="absolute right-0 bottom-2" />}
               </div>
-              <h4 className="text-foreground text-sm">{auth.user.email}</h4>
+              <div className="mb-2">
+                <div className="flex items-end gap-2">
+                  <h1 className="text-2xl">{auth.user.name}</h1>
+                  <StatusBadge type="status" status={auth.user.status} />
+                </div>
+                <h4 className="text-foreground text-sm">{auth.user.email}</h4>
+              </div>
+            </div>
+            <div>
+              <h4>Member since</h4>
+              {format(auth.user.created_at, 'PPP')}
+            </div>
+            <div className="space-y-6">
+              <Button onClick={onEditProfile}>Edit Profile</Button>
             </div>
           </div>
-          <div>
-            <h4>Member since</h4>
-            {format(auth.user.created_at, 'PPP')}
-          </div>
-          <div className="space-y-6">
-            <Button onClick={onEditProfile}>Edit Profile</Button>
+          <div className="basis-[68vw] space-y-6">
+            {hasCompanies && <CompanyList data={companies} onSelectCompany={onSelectCompany} />}
+            {hasUsers && (
+              <>
+                <Separator /> <UserList data={users} onSelectUser={onSelectUser} onAddNewUser={onAddNewUser} />
+              </>
+            )}
           </div>
         </div>
-        <div className="basis-[68vw] space-y-6">
-          {hasCompanies && <CompanyList data={companies} onSelectCompany={onSelectCompany} />}
-          {hasUsers && (
-            <>
-              <Separator /> <UserList data={users} onSelectUser={onSelectUser} onAddNewUser={onAddNewUser} />
-            </>
-          )}
-        </div>
+        <Sheet open={state.sheetState} onOpenChange={onOpenChange}>
+          <SheetContent side="right" className="m-4 flex h-[calc(100vh-2rem)] w-full flex-col rounded-md sm:max-w-[1380px] md:max-w-4xl">
+            <SheetHeader className="pb-0">
+              <SheetTitle>{t(`global.profile`)}</SheetTitle>
+              <SheetDescription className="text-[12px]">Manage your subscription and billing details</SheetDescription>
+            </SheetHeader>
+            <div className="grid gap-2 overflow-y-scroll px-4">
+              {state.sheetContent === 'company:view' && selectedCompany.company !== undefined && <Show company={selectedCompany.company} />}
+              {state.sheetContent === 'company:form' && selectedCompany.company !== undefined && (
+                <CreateCompanyForm params={selectedCompany} onFinish={modalHandler} />
+              )}
+              {['user:view', 'user:form'].includes(state.sheetContent) && (
+                <UserForm params={selectedUser} companies={companies} roles={roles} onFinish={modalHandler} />
+              )}
+              {state.sheetContent === 'profile' && <AccountForm />}
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
-
-      <Sheet open={state.sheetState} onOpenChange={onOpenChange}>
-        <SheetContent side="right" className="m-4 flex h-[calc(100vh-2rem)] w-full flex-col rounded-md md:max-w-4xl sm:max-w-[1380px]">
-          <SheetHeader className='pb-0'>
-            <SheetTitle>{t(`global.profile`)}</SheetTitle>
-            <SheetDescription className="text-[12px]">Manage your subscription and billing details</SheetDescription>
-          </SheetHeader>
-          <div className="grid gap-2 overflow-y-scroll px-4">
-            {state.sheetContent === 'company:view' && selectedCompany.company !== undefined && <Show company={selectedCompany.company} />}
-            {state.sheetContent === 'company:form' && selectedCompany.company !== undefined && (
-              <CreateCompanyForm params={selectedCompany} onFinish={modalHandler} />
-            )}
-            {['user:view', 'user:form'].includes(state.sheetContent) && (
-              <UserForm params={selectedUser} companies={companies} roles={roles} onFinish={modalHandler} />
-            )}
-            {state.sheetContent === 'profile' && <AccountForm />}
-          </div>
-        </SheetContent>
-      </Sheet>
     </AppLayout>
   );
 }
